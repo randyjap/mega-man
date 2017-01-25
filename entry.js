51741830750604cfd298cd7c258986e1989a0586
randyjap <randyjap@gmail.com>
2017-01-25 11:58:44 -0800

    Added sound and music

--- entry.js
+++ entry.js
@@ -1,10 +1,13 @@
 import { play } from './lib/game.js';
 import { levels } from './lib/levels.js';
 import { Canvas } from './lib/canvas.js';
+import { music, soundSprites } from './lib/sound.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+
+
   window.pause = false;
-  let runGame = play(levels, Canvas);
+  let runGame = play(levels, Canvas, soundSprites);
   runGame();
   let restartButton = document.getElementById("restart");
   restartButton.addEventListener("click", e => {
@@ -14,18 +17,38 @@ document.addEventListener("DOMContentLoaded", () => {
     if (canvas) {
       cancelAnimationFrame(window.animation);
       document.body.removeChild(canvas);
-      runGame = play(levels, Canvas);
+      runGame = play(levels, Canvas, soundSprites);
       runGame();
     } else {
-      runGame = play(levels, Canvas);
+      runGame = play(levels, Canvas, soundSprites);
       runGame();
     }
   });
 
-  let proxy = document.getElementById("pause");
-  proxy.addEventListener("click", e => {
+  let pause = document.getElementById("pause");
+  pause.addEventListener("click", e => {
     e.preventDefault();
     window.pause = !window.pause;
     if (!window.pause) runGame();
+  });
+
+  let music_counter = 0;
+  let muteStatus = false;
+  music[music_counter].play();
+
+  let muteButton = document.getElementById("mute");
+  muteButton.addEventListener("click", e => {
+    e.preventDefault();
+    muteStatus = !muteStatus;
+    music[music_counter].mute(muteStatus);
+    soundSprites.mute(muteStatus);
+  });
+
+  let nextButton = document.getElementById("nextSong");
+  nextButton.addEventListener("click", e => {
+    e.preventDefault();
+    music[music_counter].stop();
+    music_counter = (music_counter + 1) % 3;
+    music[music_counter].play();
   });
 });
